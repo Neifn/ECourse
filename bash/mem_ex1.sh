b5aada43d43d4ccb0b12a153407cf8bbe3c36546
neifn <alexandrshahmatov@gmail.com>
2017-08-12 20:50:49 +0300

    Lesson 4 solution

---
+++ bash/mem_ex1.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+df | sed 1d | while read line
+do
+MEMORY=$(echo $line | awk '{print $5}' | sed 's/%//')
+NAME=$(echo $line | awk '{print $1}' )
+MOUNT=$(echo $line | awk '{print $6}' )
+if (( $MEMORY >= 90 ))
+then
+    echo "Disk usage of the device $NAME ($MOUNT) is too high and is equal to ${MEMORY}%"
+else
+    echo "Disk usage of the device $NAME ($MOUNT) is within limits and is equal to ${MEMORY}%"
+fi
+done
